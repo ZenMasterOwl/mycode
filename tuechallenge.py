8cd24a6c835e95e3bbfe7aa75d4d48c818601a07
Michael Vance <michaelvance0416@gmail.com>
2020-08-11 11:45:01 -0400

    lab 11 Python Dictionaries

---
+++ tuechallenge.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+icecream= ["flavors", "salty"]
+icecream.append(99)
+name= input("Enter your name: ").title()
+
+print(f"{icecream[-1]} {icecream[0]} and {name} chooses to be {icecream[1]}.")
