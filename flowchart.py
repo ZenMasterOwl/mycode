18975a0b7432f917e3f57a3dcf173c897a7f43e8
Michael Vance <michaelvance0416@gmail.com>
2020-08-12 15:05:03 -0700

    completed flowchart

--- flowchart.py
+++ flowchart.py
@@ -1,8 +1,51 @@
  
 
+
+"""
 q to quit
 repeats question if wrong input entered
 """
+def doesWork():
+    a = input("Does the dang thing work?\n").lower().strip()
+    if a == "yes": 
+        return print("DON'T MESS WITH IT")
+    elif a == "no":
+        messedWith()
+    else:
+        doesWork()
+def messedWith():
+    a = input("Did you mess with it?\n").lower().strip()
+    if a == "yes":
+        print("You wormy scum")
+        othersAlerted()
+    elif a == "no":
+        catchHeck()
+    else:
+        messedWith()
+def othersAlerted():
+    a = input("Does anyone know?\n").lower().strip()
+    if a == "yes":
+        poorBub()
+    elif a == "no":
+        print("Hide it")
+        print("No problem")
+    else:
+        othersAlerted()
+def catchHeck():
+    a = input("Will you catch heck?\n").lower().strip()
+    if a == "yes":
+        poorBub()
+    elif a == "no":
+        print("Shoot - can it")
+        print("No problem")
+    else:
+        catchHeck()
+def poorBub():
+    print("You poor bub...")
+    a = input("Can you blame a private?\n").lower().strip()
+    if a == "yes":
+        print("No problem")
+    else:
+        poorBub()
 
-
-"""
+doesWork()
